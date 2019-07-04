2018e8ce633910ecb2db209a1834a35e60e65af3
Paula Ribeiro <paula.mribeiro05@gmail.com>
2019-07-03 22:55:07 -0300

    Comenta orquestrador

--- src/Orquestrador.cpp
+++ src/Orquestrador.cpp
@@ -7,40 +7,45 @@
 Arvore* carregarArvore() {
     Arvore* arvore = new Arvore();
 
+    //inicializa arquivo
     FILE *fp;
-
     fp = fopen("morse.txt", "r");
 
+    //valida arquivo
     if (fp == NULL) {
         std::cout << "Não foi possível abrir o arquivo com a codificação morse";
         return nullptr;
     }
 
+    //le caracter ate chegar ao fim do arquivo
     char ch;
-
     while((ch = fgetc(fp)) != EOF) {
+        //cria novo no
         No* no = new No();
         no->esquerda = nullptr;
         no->direita = nullptr;
+        //primeiro char lido representa a letra
         no->letra = ch;
 
         //le espaco entre letra e codigo
         fgetc(fp);
 
+        //le o codigo ate chegar ao fim da linha
         char codigo[10];
         int i = 0;
-
         while((ch = fgetc(fp)) != '\n' && ch != EOF) {
             codigo[i] = ch;
             i++;
         }
+        //demarca final da string de codigo
         codigo[i] = '\0';
 
         no->codigo = codigo;
-
+        //insere no na arvore
         arvore->inserir(no);
     }
 
+    //fecha arquivo
     fclose(fp);
 
     return arvore;
